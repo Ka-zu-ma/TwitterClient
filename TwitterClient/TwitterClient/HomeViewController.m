4da1000d2d94145a22597d6b66cc92d1d022dedc
Kazuma Miyazaki <miyazaki@dotinstall.com>
2016-06-28 20:56:42 +0900

    0628_プログラミング言語を含むツイートを２０件取得してきて、表示できた

--- TwitterClient/TwitterClient/HomeViewController.m
+++ TwitterClient/TwitterClient/HomeViewController.m
@@ -40,7 +40,7 @@
             _logInButton.hidden = YES;
             _tableView.hidden = NO;
             
-            [self loadTweets:session.userID];
+            [self loadTweetsOfWord:session.userID];
             
             NSLog(@"UserName : %@", session.userName);
         }
@@ -64,13 +64,54 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
+-(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-//ツイートを取得
-- (void)loadTweets:(NSString *)userId{
+//あるワードを含むツイートを取得
+-(void)loadTweetsOfWord:(NSString *)userId{
+    
+    NSString *endpoint = @"https://api.twitter.com/1.1/search/tweets.json";
+    NSDictionary *parameters = @{@"q":@"プログラミング",@"count":@"20"};
+    NSError *error = nil;
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] initWithUserID:userId];
+    NSURLRequest *request = [client URLRequestWithMethod:@"GET"
+                                                     URL:endpoint
+                                              parameters:parameters
+                                                   error:&error];
+    
+    if (error) {
+        NSLog(@"Error: %@", error);
+        return;
+    }
+    __weak typeof(self) weakSelf = self;
+    
+    [client sendTwitterRequest:request
+                    completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                        if (connectionError) {
+                            NSLog(@"Error: %@", error);
+                            return;
+                        }
+                        NSError *jsonError = nil;
+                        id jsonData = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:NSJSONReadingMutableContainers
+                                                                        error:&jsonError];
+                        if (jsonError) {
+                            NSLog(@"Error: %@", jsonError);
+                            return;
+                        }
+                        //search/tweets で返ってくるデータには直接各ツイートのデータが入っているのではなく、statusesという階層を挟んでる
+                        weakSelf.tweets = [TWTRTweet tweetsWithJSONArray:jsonData[@"statuses"]];
+                        [weakSelf.tableView reloadData];
+                    }];
+    
+}
+
+
+
+//あるUserIDのタイムラインを取得
+-(void)loadTweets:(NSString *)userId{
     
     // タイムラインを取得
     NSString *endpoint = @"https://api.twitter.com/1.1/statuses/home_timeline.json";
@@ -125,7 +166,7 @@
 
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     return [TWTRTweetTableViewCell heightForTweet:_tweets[indexPath.row] style:TWTRTweetViewStyleCompact width:_tableView.bounds.size.width showingActions:true];
     
